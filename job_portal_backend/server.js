108401b1c3d228bc924bd767dc63f9bc6cf27e38
Novanectar532 <developer.novanectar@gmail.com>
2025-06-23 09:44:10 +0530

    Update server.js

--- job_portal_backend/server.js
+++ job_portal_backend/server.js
@@ -75,7 +75,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Updated CORS configuration
 app.use(cors({
-  origin: ['https://job-portal-0dok.onrender.com','https://nnhire.novanectar.co.in','https://www.nnhire.novanectar.co.in/', 'http://localhost:5173','http://localhost:5174','http://localhost:5175'], // ⬅️ array of URLs
+  origin: ['https://job-portal-0dok.onrender.com','https://nnhire.novanectar.co.in','https://www.nnhire.novanectar.co.in', 'http://localhost:5173','http://localhost:5174','http://localhost:5175'], // ⬅️ array of URLs
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
