cfa59285d3ea289d8ac09435e33d8b5219135a31
Santosh Kumar <sk8065788@gmail.com>
2025-07-05 19:28:26 +0530

    changes

--- jobportal/src/component/Header.jsx
+++ jobportal/src/component/Header.jsx
@@ -4,15 +4,15 @@ import { FaUser, FaLock, FaBars, FaTimes } from "react-icons/fa";
 import { AuthContext } from "./AuthContext";
 import logo from './photos/logo.png'
 import { Link, useNavigate } from "react-router-dom";
-import { FaUserCircle , FaChevronDown  } from "react-icons/fa";
+import { FaUserCircle, FaChevronDown } from "react-icons/fa";
 
 const Header = () => {
-  const {user} = useContext(AuthContext);
-  console.log('u',user);
+  const { user } = useContext(AuthContext);
+  console.log('u', user);
   const [users, setUserInfo] = useState(null);
-  const [user_email , setuser_email] = useState('')
+  const [user_email, setuser_email] = useState('')
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const[pwd, setpwd]= useState('');
+  const [pwd, setpwd] = useState('');
   const [userType, setuserType] = useState('')
   // console.log('user deatil',users.userType)
   const [isOpen, setIsOpen] = useState(false);
@@ -20,30 +20,27 @@ const Header = () => {
   const [isEmployeeOpen, setIsEmployeeOpen] = useState(false);
   const [isPageOpen, setIsPageOpen] = useState(false);
   const navigate = useNavigate();
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-    
-  };
+
 
   const handleOpenCompany = () => {
-   
-    const userinfo = {user_email, pwd}; // could be JWT or any auth token
-  
+
+    const userinfo = { user_email, pwd }; // could be JWT or any auth token
+
     // Step 1: Open new tab
     const newTab = window.open("http://localhost:5174/", "_blank");
-  
+
     // Step 2: Wait a little bit and then send the token
     setTimeout(() => {
       newTab.postMessage({ userinfo }, "http://localhost:5174");
     }, 500); // wait half a second so the new tab can load
   };
 
-  const handleOpenCandidate =()=>{
-    const userinfo = {user_email, pwd}; // could be JWT or any auth token
-  
+  const handleOpenCandidate = () => {
+    const userinfo = { user_email, pwd }; // could be JWT or any auth token
+
     // Step 1: Open new tab
     const newTab = window.open("http://localhost:5175/", "_blank");
-  
+
     // Step 2: Wait a little bit and then send the token
     setTimeout(() => {
       newTab.postMessage({ userinfo }, "http://localhost:5175");
@@ -59,18 +56,18 @@ const Header = () => {
       try {
         const parsedUser = JSON.parse(userInfo);
         setUserInfo(parsedUser);
-        
+
         setuser_email(parsedUser.email)
         setuserType(parsedUser.userType);
-          // ✅ Correct
+        // ✅ Correct
       } catch (error) {
         console.error('Error parsing user info:', error);
       }
     } else {
       console.warn('No user info found in localStorage');
     }
- 
-}, [user]);
+
+  }, [user]);
 
 
   const handleLogout = () => {
@@ -83,21 +80,21 @@ const Header = () => {
 
 
   return (
-    <nav className="bg-white w-full h-2/12 shadow-md sticky flex justify-center items-center top-0 z-20">
+    <nav className="bg-[linear-gradient(130.9deg,_#C4D5FA_-6.66%,_#F7FAFC_55.55%)] w-full h-2/12 sticky flex justify-center items-center top-0 z-20">
       <div className="container mx-auto  flex justify-between items-center p-2.5 ">
         {/* Logo */}
-        <div onClick={()=>navigate('/')} className="flex items-center space-x-2">
+        <div onClick={() => navigate('/')} className="flex items-center space-x-2">
           <img src={logo} alt="Logo" className="w-10" />
-          <p className="text-2xl font-bold text-black">NN<span className="text-blue-400">Hire</span></p> 
+          <p className="text-2xl font-bold text-black">NN<span className="text-blue-400">Hire</span></p>
         </div>
 
         {/* Desktop Menu */}
         <ul className="hidden md:flex space-x-6 text-gray-700 font-medium">
-          <li onClick={()=>navigate('/')} className="hover:text-blue-500 cursor-pointer">Home</li>
-          <li onClick={()=>navigate('/job')} className="hover:text-blue-500 cursor-pointer">Find Jobs</li>
-          <li onClick={()=>navigate('/about')}  className="hover:text-blue-500 cursor-pointer">About us</li>
-      
-     
+          <li onClick={() => navigate('/')} className="hover:text-blue-500 cursor-pointer">Home</li>
+          <li onClick={() => navigate('/job')} className="hover:text-blue-500 cursor-pointer">Find Jobs</li>
+          <li onClick={() => navigate('/about')} className="hover:text-blue-500 cursor-pointer">About us</li>
+
+
 
 
           {/* Dropdown for Pages */}
@@ -125,93 +122,98 @@ const Header = () => {
 
         {/* Login & Signup */}
         <div className="hidden md:flex">
-        {users ? (
-          <div className="relative inline-block text-left">
-      <div
-        onClick={() => setDropdownOpen(!dropdownOpen)}
-        className="flex items-center gap-2 cursor-pointer border-2 border-[#4640DE] border-solid rounded-lg px-2.5 py-1.5 text-[#4640DE]"
-      >
-        <FaUserCircle size={28} className="text-[#4640DE]" />
-        <span className="text-[#4640DE] font-semibold">{user.email}</span>
-         <FaChevronDown
-          className={`text-[#4640DE] transition-transform duration-300  ${
-            dropdownOpen ? "rotate-180" : "rotate-0"
-          }`}
-        />
-      </div>
+          {users ? (
+            <div className="relative inline-block text-left">
+              <div
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className="flex items-center gap-2 cursor-pointer border-2 border-[#4640DE] border-solid rounded-lg px-2.5 py-1.5 text-[#4640DE]"
+              >
+                <FaUserCircle size={28} className="text-[#4640DE]" />
+                <span className="text-[#4640DE] font-semibold">{user.email}</span>
+                <FaChevronDown
+                  className={`text-[#4640DE] transition-transform duration-300  ${dropdownOpen ? "rotate-180" : "rotate-0"
+                    }`}
+                />
+              </div>
 
-      {dropdownOpen && (
-        <div className="absolute right-0 mt-2 w-48 border-gray-300 bg-gray-200 border border-solid rounded-md shadow-lg z-10">
-         {
-          userType === "candidate" ? (
-            <button
-              className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
-              onClick={() => window.open('/candidate_dashboard', '_blank')}
+              {dropdownOpen && (
+                <div className="absolute right-0 mt-2 w-48 border-gray-300 bg-gray-200 border border-solid rounded-md shadow-lg z-10">
+                  {
+                    userType === "candidate" ? (
+                      <button
+                        className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                        onClick={() => window.open('/candidate_dashboard', '_blank')}
               // onClick={()=>navigate('/candidate_dashboard')}
-            >
-              Candidate Dashboard
-            </button>
+                      >
+                        Candidate Dashboard
+                      </button>
+                    ) : (
+                      <button
+                        className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+                       onClick={() => window.open('/company_dashboard', '_blank')}
+                      >
+                        Recruiter Dashboard
+                      </button>
+                    )
+                  }
+                  <button
+                    className="block w-full text-left px-4 py-2 text-sm text-red-500 hover:bg-red-100"
+                    onClick={() => {
+                      handleLogout();
+                      setDropdownOpen(false);
+                    }}
+                  >
+                    Logout
+                  </button>
+                </div>
+              )}
+            </div>
           ) : (
-            <button
-              className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
-              onClick={() => window.open('/company_dashboard', '_blank')}
-            >
-              Recruiter Dashboard
-            </button>
-          )
-         }
-          <button
-            className="block w-full text-left px-4 py-2 text-sm text-red-500 hover:bg-red-100"
-            onClick={() => {
-              handleLogout();
-              setDropdownOpen(false);
-            }}
-          >
-            Logout
-          </button>
+            <div className="flex gap-4">
+              <Link to="/login" className="text-blue-500">Login</Link>
+              <Link to="/signup" className="text-green-500">Signup</Link>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-        ) : (
-          <div className="flex gap-4">
-            <Link to="/login" className="text-blue-500">Login</Link>
-            <Link to="/signup" className="text-green-500">Signup</Link>
-          </div>
-        )}
-      </div>
 
         {/* Hamburger Menu Icon */}
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-gray-700">
-            {isOpen ? <FaTimes size={25} /> : <FaBars size={25} />}
+          <button  className="text-gray-700 cursor-pointer">
+            {isOpen ? <FaTimes size={25} onClick={() => setIsOpen(false)} /> : <FaBars size={25} onClick={() => setIsOpen(true)}/> }
+
           </button>
         </div>
       </div>
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="md:hidden bg-white shadow-md">
-          <ul className="flex flex-col items-center space-y-4 py-4">
-          <div>
-        {users ? (
-          <div className="flex items-center gap-4">
-            <span>{user.email}</span>
-            <button onClick={handleLogout} className="text-red-500">Logout</button>
-          </div>
-        ) : (
-          <div className="flex gap-4">
-            <Link to="/login" className="text-blue-500">Login</Link>
-            <Link to="/signup" className="text-green-500">Signup</Link>
-          </div>
-        )}
-      </div>
-            <li className="hover:text-blue-500 cursor-pointer">Home</li>
-            <li className="hover:text-blue-500 cursor-pointer">Find Jobs</li>
-            {/* <li className="hover:text-blue-500 cursor-pointer">For Candidate</li>
+        <div className="absolute top-15 w-full">
+          <div className="md:hidden bg-white shadow-md w-full">
+
+            <ul className="flex flex-col items-center space-y-4 py-4">
+
+              <div>
+                {users ? (
+                  <div className="flex items-center gap-4">
+                    <span>{user.email}</span>
+                    <button onClick={handleLogout} className="text-red-500">Logout</button>
+                  </div>
+                ) : (
+                  <div className="flex flex-col md:flex-row items-center gap-4">
+                    <Link to="/login" className="text-blue-500">Login</Link>
+                    <Link to="/signup" className="text-green-500">Signup</Link>
+                  </div>
+                )}
+              </div>
+              <li className="hover:text-blue-500 cursor-pointer">Home</li>
+              <li className="hover:text-blue-500 cursor-pointer">Find Jobs</li>
+              {/* <li className="hover:text-blue-500 cursor-pointer">For Candidate</li>
             <li className="hover:text-blue-500 cursor-pointer">For Employee</li>
             <li className="hover:text-blue-500 cursor-pointer">Pages</li> */}
-           
-          </ul>
+
+            </ul>
+          </div>
+
         </div>
       )}
     </nav>
--- jobportal/src/component/Home/HeroSection.jsx
+++ jobportal/src/component/Home/HeroSection.jsx
@@ -1,4 +1,4 @@
-import {FaSearch,FaMapMarkerAlt,FaBriefcase,FaInstagram} from "react-icons/fa";
+import { FaSearch, FaMapMarkerAlt, FaBriefcase, FaInstagram } from "react-icons/fa";
 import React from 'react';
 import vodafone from "../photos/vodafone.png";
 import intel from "../photos/intel-3.png";
@@ -6,6 +6,7 @@ import tesla from "../photos/tesla-9.png";
 import amd from "../photos/amd-logo-1.png";
 import talkit from "../photos/talkit.png";
 import JobListings from "./JobListings";
+import hero from '../../assets/hero.svg'
 import {
   FaFacebookF,
   FaLinkedinIn,
@@ -19,64 +20,71 @@ import './css/HeroSection.css';
 const HeroSection = () => {
   return (
     <>
-      <div className="w-full h-11/12 flex-col my-auto   flex items-center justify-center overflow-hidden">
-  <div className="flex flex-col text-center w-full h-screen justify-center items-center my-auto relative -mt-12">
-        {/* Heading */}
-        <h1 className="text-4xl md:text-6xl font-semibold  text-black z-1">
-          Join us & <span className="text-blue-400">Explore</span> <br />
-          <span className="text-blue-400">Thousands</span> of Jobs
-        </h1>
-        <p className="text-gray-500 mt-8 max-w-2xl">
-          Find Jobs, Employment & Career Opportunities. Some of the companies{" "}
-          <br />
-          we've helped recruit excellent applicants over the years.
-        </p>
-
-     <div
-  className=" sticky top-0 bg-white mt-10 p-4 sm:p-6 rounded-lg flex flex-col sm:flex-row shadow-lg gap-4 justify-between items-center max-w-5xl w-full border border-gray-200 z-10 overflow-y-hidden"
-  style={{ boxShadow: "1px 1px 5px 0.5px gray" }}
->
-          <div className="flex items-center w-full sm:w-auto sm:border-r sm:border-r-gray-100 px-2 sm:px-3 ">
-            <FaSearch className="text-[#4640DE] mr-2" />
-            <input
-              type="text"
-              placeholder="Search for keywords"
-              className="outline-none border-b border-gray-400 px-2.5 border-solid  p-2 w-full"
-            />
+      <div className="bg-[linear-gradient(130.9deg,_#C4D5FA_-6.66%,_#F7FAFC_55.55%)]">
+        <div className="flex flex-col md:flex-row justify-between flex-wrap p-4 md:p-12 items-center">
+          {/* Heading */}
+          <div className="flex-1">
+                      <h1 className="text-4xl md:text-6xl font-semibold  text-black z-1">
+            Join us & <span className="text-blue-400">Explore</span> <br />
+            <span className="text-blue-400">Thousands</span> of Jobs
+          </h1>
+          <p className="text-gray-500 mt-8 max-w-2xl">
+            Find Jobs, Employment & Career Opportunities. Some of the companies{" "}
+            <br />
+            we've helped recruit excellent applicants over the years.
+          </p>
           </div>
 
-          <div className="flex items-center w-full sm:w-auto sm:border-r sm:border-r-gray-200 px-2 sm:px-3 ">
-            <FaMapMarkerAlt className="text-[#4640DE] mr-2" />
-            <input
-              type="text"
-              placeholder="Select Location"
-              className="outline-none p-2 w-full border-b border-gray-400  border-solid"
-            />
+          <div className="flex-1">
+            <img src={hero} alt="hero" className="h-[300px] md:h-full"/>
           </div>
+    </div>
+          <div className="flex flex-col text-center w-full items-center mb-12 p-4">
+          <div
+            className=" sticky top-0 bg-white mt-10 p-4 sm:p-6 rounded-lg flex flex-col sm:flex-row shadow-lg gap-4 justify-between items-center max-w-5xl w-full border border-gray-200 z-10 overflow-y-hidden"
+            style={{ boxShadow: "1px 1px 5px 0.5px gray" }}
+          >
+            <div className="flex items-center w-full sm:w-auto sm:border-r sm:border-r-gray-100 px-2 sm:px-3 ">
+              <FaSearch className="text-[#4640DE] mr-2" />
+              <input
+                type="text"
+                placeholder="Search for keywords"
+                className="outline-none border-b border-gray-400 px-2.5 border-solid  p-2 w-full"
+              />
+            </div>
 
-          <div className="flex items-center w-full sm:w-auto px-2 sm:px-3 ">
-            <FaBriefcase className="text-[#4640DE] mr-2" />
-            <input
-              type="text"
-              placeholder="Select Category"
-              className="outline-none p-2 w-full border-b border-gray-400  border-solid "
-            />
+            <div className="flex items-center w-full sm:w-auto sm:border-r sm:border-r-gray-200 px-2 sm:px-3 ">
+              <FaMapMarkerAlt className="text-[#4640DE] mr-2" />
+              <input
+                type="text"
+                placeholder="Select Location"
+                className="outline-none p-2 w-full border-b border-gray-400  border-solid"
+              />
+            </div>
+
+            <div className="flex items-center w-full sm:w-auto px-2 sm:px-3 ">
+              <FaBriefcase className="text-[#4640DE] mr-2" />
+              <input
+                type="text"
+                placeholder="Select Category"
+                className="outline-none p-2 w-full border-b border-gray-400  border-solid "
+              />
+            </div>
+
+            <button className="bg-[#4640DE] text-white font-medium py-2 px-4 rounded-md w-full sm:w-auto mt-2 sm:mt-0">
+              Search
+            </button>
           </div>
+          </div>
+      </div>
 
-          <button className="bg-[#4640DE] text-white font-medium py-2 px-4 rounded-md w-full sm:w-auto mt-2 sm:mt-0">
-            Search
-          </button>
-        </div>
-        </div>
-        </div>
+      {/* Partner Logos */}
 
-        {/* Partner Logos */}
 
-    
 
-        {/* For line and icon  */}
+      {/* For line and icon  */}
 
-        {/* <div className="w-full h-350 border-3 border-gray-200 border-dashed  rounded-full absolute top-[-135%] right-5 hidden lg:block"></div>
+      {/* <div className="w-full h-350 border-3 border-gray-200 border-dashed  rounded-full absolute top-[-135%] right-5 hidden lg:block"></div>
 
         <div className="w-[80%] h-280 border-3 border-gray-200 border-dashed rounded-full absolute top-[-110%] left-30 hidden lg:block"></div>
         <div className="w-[60%] h-250 border-3 border-gray-200 border-dashed rounded-full absolute top-[-115%] left-65 hidden lg:block"></div>
@@ -109,24 +117,24 @@ const HeroSection = () => {
         <div className="flex justify-center items-center  lg:bg-gray-300 rounded-full hover:bg-gray-400 transition duration-300 absolute lg:top-20 lg:right-110 md:top-50 md:right-30 top-22 right-25 sm:right-40">
           <FaFigma size={20} color="#0ACF83" className="m-2" />
         </div> */}
-        <JobListings/>
-      
+      <JobListings />
 
 
-      
-         <div className="relative overflow-hidden w-full mt-20 bg-white">
-  <div className="logo-slider-track flex animate-slide">
-    {[...Array(10)].map((_, i) => (
-      <div key={i} className="flex gap-10 items-center min-w-max">
-        <img src={vodafone} alt="vodafone" className="h-8 w-auto" />
-        <img src={intel} alt="intel" className="h-8 w-auto" />
-        <img src={tesla} alt="tesla" className="h-8 w-auto" />
-        <img src={amd} alt="amd" className="h-8 w-auto" />
-        <img src={talkit} alt="talkit" className="h-8 w-auto" />
+
+
+      <div className="relative overflow-hidden w-full mt-20 bg-white">
+        <div className="logo-slider-track flex animate-slide">
+          {[...Array(10)].map((_, i) => (
+            <div key={i} className="flex gap-10 items-center min-w-max">
+              <img src={vodafone} alt="vodafone" className="h-8 w-auto" />
+              <img src={intel} alt="intel" className="h-8 w-auto" />
+              <img src={tesla} alt="tesla" className="h-8 w-auto" />
+              <img src={amd} alt="amd" className="h-8 w-auto" />
+              <img src={talkit} alt="talkit" className="h-8 w-auto" />
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-  </div>
-</div>
 
     </>
   );
